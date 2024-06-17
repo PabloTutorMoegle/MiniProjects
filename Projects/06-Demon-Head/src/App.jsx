febcbe77e4260afeff60dc07806a8c4751939af5
PabloTutorMoegle <102219711+PabloTutorMoegle@users.noreply.github.com>
2024-06-18 01:00:47 +0200

    Add files via upload

---
+++ Projects/06-Demon-Head/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="helmet">
+      <div className="head">
+        <div className="eyes">
+          <div className="eye"></div>
+          <div className="eye"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
